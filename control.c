516fd78c32bb1eb36319aa98317ed3795f7fc46b
Jason Zheng <jzheng02@stuy.edu>
2020-01-03 02:14:15 -0500

    Done

--- control.c
+++ control.c
@@ -1,96 +1,37 @@
 #include "control.h"
 
-int create_semaphore() {
-    int v, r;
-    int semd = semget(KEY_SEM, 1, IPC_CREAT | IPC_EXCL | 0644);
-    if(semd == -1) {
-        semd = semget(KEY_SEM, 1, 0);
-        v = semctl(semd, 0, GETVAL, 0);
-    } else {
-        union semun us;
-        us.val = 1;
-        r = semctl(semd, 0, SETVAL, us);
+int main(int argc, char * argv[]) {
+    if (argc > 1) {
+        if(strcmp(argv[1], "-c") == 0) {
+            create_semaphore();
+            create_shared_memory();
+            create_file(FILE_NAME);
+        }
+        else if (strcmp(argv[1], "-r") == 0) {
+            int semd = semget(KEY_SEM, 1, 0);
+
+            struct sembuf sb;
+            sb.sem_num = 0;
+            sb.sem_op = -1;
+            printf("trying to get in\n");
+            semop(semd, &sb, 1);
+
+            read_file(FILE_NAME);
+
+            int shmd = shmget(KEY_SHM, SEG_SIZE, 0600);
+
+            remove_shared_memory(shmd);
+            remove_file(FILE_NAME);
+            remove_semaphore(semd);
+
+        }
+        else if (strcmp(argv[1],"-v") == 0 ) {
+            read_file(FILE_NAME);
+        }
     }
-    printf("semaphore created\n");
-    return semd;
-}
-
-int remove_semaphore(int semd) {
-    semctl(semd, IPC_RMID, 0);
-    printf("semaphore removed\n");
-    return 0;
-}
-
-int create_file(char * name) {
-    int fd;
-    fd = open(name, O_CREAT | O_EXCL | O_TRUNC, 0644);
-    if (fd < 0) {
-        fd = open(name, O_TRUNC);
+    else{
+        printf("Please attach one of the 3 tags [-c, -r, -v]\n" );
     }
-    close(fd);
-    printf("file created\n");
-    return fd;
-}
-
-int remove_file(char * name) {
-    if (remove(name) == 0) {
-        printf("file removed\n");
-    } else {
-        printf("File not removed\n");
-    }
-    return 0;
-}
-
-int read_file(char * name) {
-    int fd;
-    fd = open(name, O_RDONLY);
-    if(fd < 0) {
-        printf("error %d: %s\n", errno, strerror(errno));
-        return 1;
-    }
-    printf("The story so far:\n");
-    char buff[SEG_SIZE];
-    while(read(fd, buff, SEG_SIZE) > 0) {
-        printf("%s\n", buff);
-    }
-    close(fd);
-}
-
-int create_shared_memory() {
-    int shmd;
-    shmd = shmget(KEY_SHM, SEG_SIZE, IPC_CREAT | 0600);
-    printf("shared memory created\n");
-    return shmd;
-}
-
-int remove_shared_memory(int shmd) {
-    shmctl(shmd, IPC_RMID, 0);
-    printf("shared memory removed\n");
-    return 0;
-}
-
-int write_line() {
-    int shmd;
-    char * data;
-
-    shmd = shmget(KEY_SHM, SEG_SIZE, 0600);
-    if(shmd < 0) {
-        printf("error %d: %s\n", errno, strerror(errno));
-        return 1;
-    }
-
-    data = shmat(shmd, 0, 0);
-    printf("Last addition: %s\n", data);
-
-    printf("Your addition: ");
-    fgets(data, SEG_SIZE, stdin);
-    *strchr(data, '\n') = 0;
-
-    int fd = open(FILE_NAME, O_WRONLY | O_APPEND);
-    write(fd, data, SEG_SIZE);
-    close(fd);
-
-    shmdt(data);
 
     return 0;
 }
--- makefile
+++ makefile
@@ -1,18 +1,18 @@
-all: main.o write.o main.o control.o
-	gcc -o main main.o control.o
-	gcc -o write write.o control.o
+all: control.o write.o control.o control_func.o
+	gcc -o control control.o control_func.o
+	gcc -o write write.o control_func.o
 
-main.o: main.c control.h control.o
-	gcc -c main.c
+control.o: control.c control_func.h control_func.o
+	gcc -c control.c
 
-write.o: write.c control.h control.o
+write.o: write.c control_func.h control_func.o
 	gcc -c write.c
 
-control.o: control.c control.h
-	gcc -c control.c
+control_func.o: control_func.c control_func.h
+	gcc -c control_func.c
 
 clean:
 	rm *o
-	rm main
+	rm control
 	rm write
 	rm story
